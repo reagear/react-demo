b76dfd1326cd0cac653852607ceb3816ed8573b2
lijie <lijie@weidian.com>
2020-04-07 14:39:45 +0800

    init

---
+++ .stylelintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: 'stylelint-config-standard',
+    rules: {
+        indentation: null,
+        'property-no-unknown': [
+            true,
+            {
+                ignoreProperties: ['composes']
+            }
+        ],
+        'selector-pseudo-class-no-unknown': null,
+        'selector-list-comma-newline-after': null,
+        'unit-no-unknown': [
+            true,
+            {
+                ignoreUnits: ['rpx']
+            }
+        ],
+        'declaration-empty-line-before': null,
+        'rule-empty-line-before': null
+    }
+};
