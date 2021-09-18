0cbeb822438c6fd3c3d64a7f74a0060ff95e832e
jie2.li <jie2.li@rootcloud.com>
2021-09-18 09:52:47 +0800

    测试styled-jsx内含js变量功能

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['stylelint-config-standard','stylelint-config-recess-order'],
+    extends: ['stylelint-config-standard','stylelint-config-recess-order','stylelint-config-css-modules'],
     rules: {
         indentation: null,
         'property-no-unknown': [
--- src/pages/list/components/common-test/index.jsx
+++ src/pages/list/components/common-test/index.jsx
@@ -2,27 +2,48 @@ import React from 'react';
 import css from 'styled-jsx/css';
 
 const { className, styles } = css.resolve`
-    div :global(.ss) {
-        color: red;
-    }
-
     :global(p) {
         color: #eee;
+    }
+
+    div :global(.ss) {
+        color: red;
     }
 `;
 
 const InnerComp = ({ a, b, cn = '' }) => {
+    // 1. value 中间含js ok
+    // 2. key 含js ok
+    // 3. key 是js ok
+    // 4. rule是 js ok
+    // 5. block 是js error
+
+    const css1 = '#000';
+    const css2 = 'font';
+    const css3 = 'color';
+    const css4 = `cursor:auto`;
+    const css5 = `p {
+                        font-size: 30px;
+                        color: #a8d52d;
+                    }`// 语法错误
+
     return (
         <div className={cn}>
             <p>a:{a}</p>
             <p>b:{b}</p>
-            <div className="ss">13</div>
+            <div className='ss'>13</div>
             <style jsx>
                 {`
+
+
                     p {
-                        font-size: 30px;
-                        color: #a8d52d;
+                        border: 1px solid ${css1};
+                        ${css2}-size: 12px;
+                        ${css3}: red;
+                        ${css4}
                     }
+
+                    ${css5}
                 `}
             </style>
         </div>
@@ -38,7 +59,7 @@ const Comp = () => {
     return (
         <div>
             <p>外层组件</p>
-            <InnerComp {...props} a="正常a" b="正常b" cn={className} />
+            <InnerComp {...props} a='正常a' b='正常b' cn={className} />
             {styles}
         </div>
     );
